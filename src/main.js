c8818a97d537f21a24b924bde9b71e753c0fa553
Thiago Soares <thiagocarv4@gmail.com>
2025-02-19 19:28:19 -0300

    instalando dependencias e configurando ambiente

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
-const openButton = document.querySelector(".open-modal")
-const closeButton = document.querySelector(".close-modal")
-const title = document.querySelector(".title")
-const schedulesByPeriod = document.querySelector(".schedules-by-period")
+"use strict"
 
-openButton.addEventListener("click", () => {
-  const modalId = openButton.getAttribute('data-modal');
-  const modal = document.getElementById(modalId)
-
-  modal.showModal();
-  title.classList.add("hide-schedules")
-  schedulesByPeriod.classList.add("hide-schedules")
-  openButton.classList.add("hide-button")
-})
-
-closeButton.addEventListener("click", () => {
-  const modalId = openButton.getAttribute('data-modal');
-  const modal = document.getElementById(modalId)
-
-  modal.close()
-  title.classList.remove("hide-schedules")
-  schedulesByPeriod.classList.remove("hide-schedules")
-  openButton.classList.remove("hide-button")
-})
+//css
+import "./styles/form/form.css"
+import "./styles/form/input.css"
+import "./styles/btn.css"
+import "./styles/global.css"
+import "./styles/header.css"
+import "./styles/logo.css"
+import "./styles/modal.css"
+import "./styles/schedules.css"
+import "./styles/utility.css"
